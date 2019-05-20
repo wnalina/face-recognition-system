b162b4169103b587a379ecb5be5ce022e7413a7d
NALINA <w_nalina@hotmail.com>
2019-05-20 15:55:59 +0700

    delete comment

--- application/views/header_profile.php
+++ application/views/header_profile.php
@@ -29,34 +29,8 @@
 
 <body id="page-top">
 
-<!-- Navigation-->
-<!--<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">-->
-<!--    <div class="container">-->
-<!--        <a class="navbar-brand js-scroll-trigger" href="#page-top">HOME</a>-->
-<!--        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--            <span class="navbar-toggler-icon"></span>-->
-<!--        </button>-->
-<!--        <div class="collapse navbar-collapse" id="navbarResponsive">-->
-<!--            <ul class="navbar-nav ml-auto my-2 my-lg-0">-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link js-scroll-trigger" href="#about">Sign in</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link js-scroll-trigger" href="#services">Sign up</a>-->
-<!--                </li>-->
-                <!--                <li class="nav-item">-->
-                <!--                    <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>-->
-                <!--                </li>-->
-                <!--                <li class="nav-item">-->
-                <!--                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>-->
-                <!--                </li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</nav>-->
-
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3navbar  sticky-top bg-nav flex-md-nowrap p-0 navbar-expand-md fixed-top py-3">
-<!--    <div class="container">-->
+
         <a class="navbar-brand js-scroll-trigger text-white nav-bar" href="#page-top">HOME</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -67,49 +41,13 @@
                     <?php $email = $this->session->email; ?>
                     <a class="nav-link js-scroll-trigger text-white" ><?=$email?></a>
                 </li>
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link js-scroll-trigger text-white" href="#services">Sign up</a>-->
-<!--                </li>-->
-                <!--                <li class="nav-item">-->
-                <!--                    <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>-->
-                <!--                </li>-->
-                <!--                <li class="nav-item">-->
-                <!--                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>-->
-                <!--                </li>-->
             </ul>
         </div>
-<!--    </div>-->
 </nav>
 
-<!--<nav class="navbar navbar-dark sticky-top bg-nav flex-md-nowrap p-0 navbar-expand-md">-->
-<!--    <div class="navbar-brand d-flex align-items-center justify-content-between col-sm-12 col-md-2 mr-0">-->
-<!--        <a class="mr-1 d-md-none text-light" href="#sidenav" data-toggle="collapse" data-target="#sidenav">-->
-<!--            <span data-feather="menu" class="my-1"></span>-->
-<!--        </a>-->
-<!--        <a class="text-light" href="">Company</a>-->
-<!--        <a class="navbar-toggler border-0 p-0" href="#topnav" data-toggle="collapse" data-target="#topnav">-->
-<!--            <span data-feather="menu" class="my-1"></span>-->
-<!--        </a>-->
-<!--    </div>-->
-<!--    <div class="navbar-collapse collapse" id="topnav"">-->
-        <!--        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">-->
-<!--        <ul class="navbar-nav px-3 py-2">-->
-<!--            <li class="nav-item text-nowrap " >-->
-<!--                <a class="nav-link"></a>-->
-<!--            </li>-->
-<!--        </ul>-->
-<!--    </div>-->
-<!---->
-<!--    <ul class="navbar-nav px-3 py-2">-->
-<!--        <li class="nav-item text-nowrap " >-->
-<!--            <a class="nav-link" href="#">Sign out</a>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</nav>-->
 
 <div class="container-fluid">
     <div class="row navbar-expand-md">
-<!--        <nav class="col-md-3 col-lg-2 bg-light navbar-collapse collapse sidebar" id="sidenav">-->
             <div id="sidebar-container" class="sidebar-expanded  d-md-block">
                 <ul class="list-group ">
                     <!-- Bootstrap List Group -->
@@ -131,80 +69,8 @@
                     <a href="<?=base_url('profile/logout')?>" data-tooltip="true" data-placement="top" title="" aria-expanded="false" class="bg-mac list-group-item list-group-item-action flex-column align-items-start">
                         <div class="d-flex w-100 justify-content-start align-items-center"><span class="fas fa-sign-out-alt fa-fw mr-3"></span><span class="menu-collapsed">Sign out</span><span class=" ml-auto"></span></div>
                     </a>
-<!--                    <div id="submenu2" class="collapse sidebar-submenu">-->
-<!--                        <a href="newProject.php" data-tooltip="true" data-placement="top" title="Creat new project" class="bg-mac list-group-item list-group-item-action"><span class="menu-collapsed">new Project</span></a>-->
-<!--                        <a href="sub02link" data-tooltip="true" data-placement="top" title="sub02tooltip" class="bg-mac list-group-item list-group-item-action"><span class="menu-collapsed">sub02title</span></a>-->
-<!--                    </div>-->
-<!--                    <li class="sidebar-separator menu-collapsed"></li>-->
-<!--                    <a href="sub03link" data-tooltip="true" data-placement="top" title="tooltip1" class="bg-mac list-group-item list-group-item-action">-->
-<!--                        <div class="d-flex w-100 justify-content-start align-items-center"><span class="fas fa-tachometer-alt fa-fw mr-3"></span><span class="menu-collapsed">title1</span></div>-->
-<!--                    </a>-->
-<!--                    <a href="#" data-toggle="sidebar-collapse" class="bg-mac list-group-item list-group-item-action d-flex align-items-center">-->
-<!--                        <div class="d-flex w-100 justify-content-start align-items-center"><span id="collapse-icon" class="fa fa-2x mr-2 text-warning"></span><span id="collapse-text" class="menu-collapsed">Close Sidebar</span></div>-->
-<!--                    </a>-->
 
                 </ul>
-<!--                <ul class="nav flex-column">-->
-<!--                    <li class="nav-item">-->
-<!--                        <h5 class="nav-link active" style="color: orangered" href="#">-->
-<!--                            <span data-feather="home "></span> Dashboard <span class="sr-only">(current)</span>-->
-<!--                        </h5>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="file"></span> Orders-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="shopping-cart"></span> Products-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="users"></span> Customers-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="bar-chart-2"></span> Reports-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="layers"></span> Integrations-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                </ul>-->
-
-<!--                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">-->
-<!--                    <span>Saved reports</span>-->
-<!--                    <a class="d-flex align-items-center text-muted" href="#">-->
-<!--                        <span data-feather="plus-circle"></span>-->
-<!--                    </a>-->
-<!--                </h6>-->
-<!--                <ul class="nav flex-column mb-2">-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="file-text"></span> Current month-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="file-text"></span> Last quarter-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="file-text"></span> Social engagement-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link" href="#">-->
-<!--                            <span data-feather="file-text"></span> Year-end sale-->
-<!--                        </a>-->
-<!--                    </li>-->
-<!--                </ul>-->
             </div>
 <!--        </nav>-->
 
